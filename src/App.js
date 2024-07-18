75967d6458559660cff94ad9a47339123c704c3d
bohli donia <bohli.donia@gmail.com$ git config --global user.email>
2024-07-18 15:01:28 +0200

    premier commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from "react";
+import "./styles.css";
+import TaskForm from "./TaskForm";
+import TasksHeader from "./TasksHeader";
+import TasksList from "./TasksList";
+import { Provider } from 'react-redux';
+import { store } from './redux';
+
+export default function App() {
+  return (
+    < Provider store={store}>
+    <div className="container">
+      <article>
+        <TasksHeader  />
+        <TasksList />
+        <footer>
+          <TaskForm  />
+        </footer>
+      </article>
+    </div>
+    </Provider>
+
+  );
+}
+
